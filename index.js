3020d9932527da89fb8a7b434d8b00ed97babb14
Sohrabrustami <sohrab.rustami@outlook.com>
2026-01-26 19:11:12 +0100

    password generater webpage files

---
+++ index.js
@@ -0,0 +1,60 @@
+
+const characters = [
+  // Uppercase A–Z
+  "A","B","C","D","E","F","G","H","I","J","K","L","M",
+  "N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+
+  // Lowercase a–z
+  "a","b","c","d","e","f","g","h","i","j","k","l","m",
+  "n","o","p","q","r","s","t","u","v","w","x","y","z",
+
+  // Digits 0–9
+  "0","1","2","3","4","5","6","7","8","9",
+
+  // Special characters
+  "!","@","#","$","%","^","&","*","(",")",
+  "-","_","=","+","[","]","{","}",";",":",
+  "'","\"",",",".","<",">","/","?","\\","|","`","~"
+];
+
+
+
+
+let generated_password = document.getElementById("gn_btn")
+
+let generated_password2 = document.getElementById("gn2_btn")
+
+
+
+function getRandomNum(){
+  
+    let indexN = Math.floor(Math.random()* characters.length)
+    return characters[indexN]
+
+}
+
+
+
+function generatePass(){
+    let list= ''
+    for(let i=0;i<8;i++){
+        list+=getRandomNum()
+    }
+    generated_password.innerText = list
+    
+}
+
+
+
+function generatePass2(){
+    let list2= ''
+    for(let i=0;i<8;i++){
+        list2+=getRandomNum()
+    }
+    generated_password2.innerText = list2
+    
+}
+
+
+
+
